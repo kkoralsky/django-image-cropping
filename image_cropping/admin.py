75acb48ac006e7e4c10ba9e73e4ac29f3f62249f
Karol Ossowski <karol.ossowski@gmail.com>
2017-02-03 16:14:22 +0100

    django >=1.10 compat. fix

--- image_cropping/admin.py
+++ image_cropping/admin.py
@@ -4,10 +4,10 @@ from .utils import get_backend
 
 
 class ImageCroppingMixin(object):
-    def formfield_for_dbfield(self, db_field, **kwargs):
+    def formfield_for_dbfield(self, db_field, *args, **kwargs):
         crop_fields = getattr(self.model, 'crop_fields', {})
         if db_field.name in crop_fields:
             target = crop_fields[db_field.name]
             kwargs['widget'] = get_backend().get_widget(db_field, target, self.admin_site)
 
-        return super(ImageCroppingMixin, self).formfield_for_dbfield(db_field, **kwargs)
+        return super(ImageCroppingMixin, self).formfield_for_dbfield(db_field, *args, **kwargs)
